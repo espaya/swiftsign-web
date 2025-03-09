57676cc0a1304f8122a5156d286a80d0415b2a99
espaya <beehivelive8@gmail.com>
2025-03-09 21:51:47 +0000

    Edited the url

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,10 +1,9 @@
-
 <footer class="footer-wrapper">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="footer-copyright">
-                            <p>{{ date('Y') }} @<a href="#">Swift Sign</a>
+                            <p>{{ date('Y') }} @<a href="{{ url('/') }}">SwiftSign</a>. All Rights Reserved
                             </p>
                         </div>
                     </div>
