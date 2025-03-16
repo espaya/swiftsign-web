b0ef87ec1fb5881de5a761f3b26eec21e4ff3c2c
espaya <beehivelive8@gmail.com>
2025-03-16 22:52:13 +0000

    Removed button

--- resources/views/dashboard/dashboard-attendance.blade.php
+++ resources/views/dashboard/dashboard-attendance.blade.php
@@ -7,10 +7,11 @@
       <title>SwiftSign - Attendance</title>
       <link href="../../../../css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
       <meta name="csrf-token" content="{{ csrf_token() }}">
-
       <!-- inject:css-->
       <link rel="stylesheet" href="{{asset('css/plugin.min.css')}}">
       <link rel="stylesheet" href="{{asset('style.css')}}">
+      <!-- Include DataTables CSS -->
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
       <!-- endinject -->
       <link rel="icon" type="image/png" sizes="16x16" href="{{asset('SwiftSign Web.png')}}">
    </head>
@@ -40,122 +41,9 @@
                               <input class="form-control mr-sm-2 border-0 box-shadow-none" type="search" placeholder="Search by Name" aria-label="Search">
                            </form>
                         </div>
-                        <div class="action-btn">
-                           <a href="#" class="btn px-15 btn-primary" data-toggle="modal" data-target="#new-member">
-                           <i class="las la-plus fs-16"></i>Add New Member</a>
-                           <!-- Modal -->
-                           <div class="modal fade new-member" id="new-member" role="dialog" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                              <div class="modal-dialog modal-dialog-centered">
-                                 <div class="modal-content  radius-xl">
-                                    <div class="modal-header">
-                                       <h6 class="modal-title fw-500" id="staticBackdropLabel">Create project</h6>
-                                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                       <span data-feather="x"></span>
-                                       </button>
-                                    </div>
-                                    <div class="modal-body">
-                                       <div class="new-member-modal">
-                                          <form>
-                                             <div class="form-group mb-20">
-                                                <input type="text" class="form-control" placeholder="Duran Clayton">
-                                             </div>
-                                             <div class="form-group mb-20">
-                                                <div class="category-member">
-                                                   <select class="js-example-basic-single js-states form-control" id="category-member">
-                                                      <option value="JAN">1</option>
-                                                      <option value="FBR">2</option>
-                                                   </select>
-                                                </div>
-                                             </div>
-                                             <div class="form-group mb-20">
-                                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Project description"></textarea>
-                                             </div>
-                                             <div class="form-group textarea-group">
-                                                <label class="mb-15">status</label>
-                                                <div class="d-flex">
-                                                   <div class="project-task-list__left d-flex align-items-center">
-                                                      <div class="checkbox-group d-flex mr-50 pr-10">
-                                                         <div class="checkbox-theme-default custom-checkbox checkbox-group__single d-flex">
-                                                            <input class="checkbox" type="checkbox" id="check-grp-1" checked="">
-                                                            <label for="check-grp-1" class="fs-14 color-light strikethrough">
-                                                            status
-                                                            </label>
-                                                         </div>
-                                                      </div>
-                                                      <div class="checkbox-group d-flex mr-50 pr-10">
-                                                         <div class="checkbox-theme-default custom-checkbox checkbox-group__single d-flex">
-                                                            <input class="checkbox" type="checkbox" id="check-grp-2">
-                                                            <label for="check-grp-2" class="fs-14 color-light strikethrough">
-                                                            Deactivated
-                                                            </label>
-                                                         </div>
-                                                      </div>
-                                                      <div class="checkbox-group d-flex">
-                                                         <div class="checkbox-theme-default custom-checkbox checkbox-group__single d-flex">
-                                                            <input class="checkbox" type="checkbox" id="check-grp-3">
-                                                            <label for="check-grp-3" class="fs-14 color-light strikethrough">
-                                                            bloked
-                                                            </label>
-                                                         </div>
-                                                      </div>
-                                                   </div>
-                                                </div>
-                                             </div>
-                                             <div class="mb-25">
-                                                <div class="form-group mb-10">
-                                                   <label for="name47">project member</label>
-                                                   <input type="text" class="form-control" id="name47" placeholder="Search members">
-                                                </div>
-                                                <ul class="d-flex flex-wrap mb-20 user-group-people__parent">
-                                                   <li>
-                                                      <a href="#"><img class="rounded-circle wh-34" src="img/tm1.png" alt="author"></a>
-                                                   </li>
-                                                   <li>
-                                                      <a href="#"><img class="rounded-circle wh-34" src="img/tm2.png" alt="author"></a>
-                                                   </li>
-                                                   <li>
-                                                      <a href="#"><img class="rounded-circle wh-34" src="img/tm3.png" alt="author"></a>
-                                                   </li>
-                                                   <li>
-                                                      <a href="#"><img class="rounded-circle wh-34" src="img/tm4.png" alt="author"></a>
-                                                   </li>
-                                                   <li>
-                                                      <a href="#"><img class="rounded-circle wh-34" src="img/tm5.png" alt="author"></a>
-                                                   </li>
-                                                </ul>
-                                             </div>
-                                             <div class="d-flex new-member-calendar">
-                                                <div class="form-group w-100 mr-sm-15 form-group-calender">
-                                                   <label for="datepicker">start Date</label>
-                                                   <div class="position-relative">
-                                                      <input type="text" class="form-control" id="datepicker" placeholder="mm/dd/yyyy">
-                                                      <a href="#">
-                                                      <span data-feather="calendar"></span></a>
-                                                   </div>
-                                                </div>
-                                                <div class="form-group w-100 form-group-calender">
-                                                   <label for="datepicker2">End Date</label>
-                                                   <div class="position-relative">
-                                                      <input type="text" class="form-control" id="datepicker2" placeholder="mm/dd/yyyy">
-                                                      <a href="#">
-                                                      <span data-feather="calendar"></span></a>
-                                                   </div>
-                                                </div>
-                                             </div>
-                                             <div class="button-group d-flex pt-25">
-                                                <button class="btn btn-primary btn-default btn-squared text-capitalize">add new project
-                                                </button>
-                                                <button class="btn btn-light btn-default btn-squared fw-400 text-capitalize b-light color-light">cancel
-                                                </button>
-                                             </div>
-                                          </form>
-                                       </div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                           <!-- Modal -->
-                        </div>
+
+                        
+
                      </div>
                      <!-- Message Box for Feedback -->
                      <div id="message-box"></div><br>
@@ -289,6 +177,7 @@
       <!-- inject:js-->
       <script src="{{asset('js/plugins.min.js')}}"></script>
       <script src="{{asset('js/script.min.js')}}"></script>
+      <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
       <!-- endinject-->
       <script>
          function fetchAttendanceData() {
@@ -329,14 +218,14 @@
                              <td><div class="userDatatable-content">${record.signed_out_at}</div></td>
                              <td>
                                  <div class="userDatatable-content d-inline-block
-                                             ${record.expired === 'Yes' ? 'alert-danger text-danger' : 'alert-success text-success'} 
+                                             ${record.expired === 'YES' ? 'alert-danger text-danger' : 'alert-success text-success'} 
                                              rounded-pill p-1 text-center">
                                      ${record.expired}
                                  </div>
                              </td>
                              <td>
                                  <div class="userDatatable-content d-inline-block">
-                                     <span class="bg-opacity-${record.status === 'SIGNED' ? 'success' : 'danger'} color-${record.status === 'SIGNED' ? 'success' : 'info'} rounded-pill userDatatable-content-status">${record.status}</span>
+                                     <span class="bg-opacity-${record.status === 'SIGNED' ? 'success' : 'danger'} color-${record.status === 'SIGNED' ? 'success' : 'danger'} rounded-pill userDatatable-content-status">${record.status}</span>
                                  </div>
                              </td>
                              <td>
